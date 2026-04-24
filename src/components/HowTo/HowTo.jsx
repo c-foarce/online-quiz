d4ba46a728e114b83dc83bf1cda2ac682d3499e4
c-foarce <dimensionripper@gmail.com>
2026-04-24 15:28:19 +0100

    refactor: centralise module CSS into shared styles directory

--- src/components/HowTo/HowTo.jsx
+++ src/components/HowTo/HowTo.jsx
@@ -1,4 +1,14 @@
+//style module import
+import styles from './HowTo.module.css'
+
 function HowTo() {
+    return (
+        <>
+        <h1 className={styles.title}>This is placeholder text, expand later</h1>
+        <p>------</p>
+        
+        </>
+    )
 
 }
 export default HowTo
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,9 +1,15 @@
+
+import buttonStyles from '../../styles/button.module.css'
+import textStyles from '../../styles/typography.module.css'
 import styles from './Welcome.module.css'
 
 function Welcome () {
     return (
         <>
-        <h1>Welcome to my Zelda Quiz! Please choose an option:</h1></>
+        <h1 className={textStyles.title}>Welcome to my Zelda Quiz! Please choose an option:</h1>   
+        <button className ={buttonStyles.startButton} onClick = {()=> alert("Hello")}>Test 1</button>
+        <button>Test 2</button>
+             </>
     )
 }
 export default Welcome
