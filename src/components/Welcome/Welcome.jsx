fc368f3985670774dee250546c078af73be0ccc1
c-foarce <dimensionripper@gmail.com>
2026-04-25 00:38:09 +0100

    clsx install and import, minor testing with basic buttons

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,15 +1,18 @@
-
+//importing styles from module sheets and from a central styling directory
 import buttonStyles from '../../styles/button.module.css'
 import textStyles from '../../styles/typography.module.css'
 import styles from './Welcome.module.css'
 
-function Welcome () {
+//to help with applying various modules to one element easier, clsx import
+import clsx from "clsx"
+
+function Welcome() {
     return (
         <>
-        <h1 className={textStyles.title}>Welcome to my Zelda Quiz! Please choose an option:</h1>   
-        <button className ={buttonStyles.startButton} onClick = {()=> alert("Hello")}>Test 1</button>
-        <button>Test 2</button>
-             </>
+            <h1 className={textStyles.title}>Welcome to my Zelda Quiz! Please choose an option:</h1>
+            <button className={clsx(buttonStyles.button, buttonStyles.startButton)} onClick={() => alert("Hello")}>Test 1</button>
+            <button className={clsx(buttonStyles.button, buttonStyles.helpButton)}>Test 2</button>
+        </>
     )
 }
 export default Welcome
